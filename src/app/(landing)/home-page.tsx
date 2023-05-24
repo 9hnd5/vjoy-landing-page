c610353d66e75f89b9e9f70e0a8542665a07b4b7
Tran Nguyen Huu Phuc <phuctran@vus-etsc.edu.vn>
2023-05-24 09:58:12 +0700

    fix NextRouter was not mounted

--- src/app/(landing)/home-page.tsx
+++ src/app/(landing)/home-page.tsx
@@ -1,6 +1,28 @@
 "use client";
 
+import { useRouter } from "next/router";
+import { useEffect } from "react";
+import * as gtag from "@/utils/gtag";
+
 export default function HomePage({ docs }: any) {
+
+  const router = useRouter()
+
+  useEffect(() => {
+    const handleRouteChange = (url: string) => {
+      gtag.pageview(url)
+    }
+    //When the component is mounted, subscribe to router changes
+    //and log those page views
+    router.events.on('routeChangeComplete', handleRouteChange)
+
+    // If the component is unmounted, unsubscribe
+    // from the event with the `off` method
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange)
+    }
+  }, [router.events])
+
   return (
     <div>
       <h1>Home page</h1>
