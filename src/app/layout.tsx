8e8e976066ec8ceff74f4fae1d1b955d613fe580
Tran Nguyen Huu Phuc <phuctran@vus-etsc.edu.vn>
2023-05-24 13:40:15 +0700

    update use gtag

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 // import "./globals.css";
 import { Inter } from "next/font/google";
 import { Providers } from "./providers";
+import Script from "next/script";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -20,6 +21,21 @@ export default function RootLayout({
         className={inter.className}
         style={{ height: "100%", width: "100%" }}
       >
+        <Script
+          async
+          src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+        />
+        <Script
+          id="gtag"
+          strategy="lazyOnload"
+          dangerouslySetInnerHTML={{
+            __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+          `,
+          }}
+        />
         <Providers>{children}</Providers>
       </body>
     </html>
