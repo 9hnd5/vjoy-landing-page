b26ab1ce2a8815bebb506f6f99c978d15e7fed0b
Tran Nguyen Huu Phuc <phuctran@vus-etsc.edu.vn>
2023-05-24 14:34:01 +0700

    fix gtag is not function

--- src/utils/gtag.ts
+++ src/utils/gtag.ts
@@ -1,4 +1,7 @@
 export const pageview = (url: string) => {
+  if (typeof window === "undefined") return;
+  if (typeof window.gtag === "undefined") return;
+
   window.gtag("config", `'${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}'`, {
     page_path: url,
   });
@@ -13,6 +16,9 @@ type GTagEvent = {
 
 // https://developers.google.com/analytics/devguides/collection/gtagjs/events
 export const event = ({ action, category, label, value }: GTagEvent): void => {
+  if (typeof window === "undefined") return;
+  if (typeof window.gtag === "undefined") return;
+
   window.gtag("event", action, {
     event_category: category,
     event_label: label,
