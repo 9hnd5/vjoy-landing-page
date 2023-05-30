94fdb5492de54b3c4eca265263495207d9d8db64
Tran Nguyen Huu Phuc <phuctran@vus-etsc.edu.vn>
2023-05-30 10:57:30 +0700

    restructure

---
+++ src/components/play-to-earn.tsx
@@ -0,0 +1,4 @@
+"use client";
+export default function PlayToEarn() {
+  return <div>Play to earn</div>;
+}
---
+++ src/components/play-to-learn.tsx
@@ -0,0 +1,4 @@
+"use client";
+export default function PlayToLearn() {
+  return <div>Play to learn</div>;
+}
