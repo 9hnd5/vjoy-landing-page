77992a21b048c156617062abc8e2b69c60f74037
Tran Nguyen Huu Phuc <phuctran@vus-etsc.edu.vn>
2023-06-09 15:16:35 +0700

    add slider, prepare for v2

--- src/app/components/PlayToLearn.tsx
+++ src/app/components/PlayToLearn.tsx
@@ -1,7 +1,12 @@
 "use client";
 
-import { Box, Flex, Heading, Text } from "@chakra-ui/react";
+import { Box, Flex, Heading, Text, useMediaQuery } from "@chakra-ui/react";
 import Image from "next/image";
+
+import React, { use } from "react";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick-theme.css";
+import "slick-carousel/slick/slick.css";
 
 export type PlayToLearnItem = {
   img: string;
@@ -16,9 +21,24 @@ interface PlayToLearnProps {
 }
 
 export default function PlayToLearn({ items }: PlayToLearnProps) {
+  const [activeIndex, setActiveIndex] = React.useState(0);
+  const [isMobile] = useMediaQuery("(max-width: 480px)");
+  const settings = {
+    autoplay: true,
+    autoplaySpeed: 3000,
+    centerMode: true,
+    draggable: true,
+    focusOnSelect: true,
+    infinite: true,
+    slidesToShow: 1,
+    centerPadding: "200px",
+    speed: 500,
+    beforeChange: (_oldIndex: number, newIndex: number) =>
+      setActiveIndex(newIndex),
+  };
   return (
     <Flex w="full" justify="space-around" my={10}>
-      <Flex w="full" maxW="1280px" direction="column" px={{ base: 8, sm: 0 }}>
+      <Flex w="full" direction="column" px={{ base: 8, sm: 0 }}>
         <Flex
           w="full"
           direction="column"
@@ -28,12 +48,27 @@ export default function PlayToLearn({ items }: PlayToLearnProps) {
         >
           <Image width={300} height={300} alt="Logo" src="/logo.svg" priority />
           <Heading size="lg" fontWeight="bold" my={5}>
-            Con vui học và sáng tạo trong từng hoạt động
+            Con vui học, sáng tạo trong từng hoạt động
           </Heading>
         </Flex>
-        {items.map((item, index) => (
-          <Item key={index} index={index} {...item} />
-        ))}
+        {isMobile &&
+          items.map((item, index) => (
+            <Item key={index} index={index} isMobile={isMobile} {...item} />
+          ))}
+
+        {!isMobile && (
+          <Slider {...settings}>
+            {items.map((item, index) => (
+              <Item
+                key={index}
+                index={index}
+                isMobile={isMobile}
+                activeIndex={activeIndex}
+                {...item}
+              />
+            ))}
+          </Slider>
+        )}
       </Flex>
     </Flex>
   );
@@ -41,41 +76,59 @@ export default function PlayToLearn({ items }: PlayToLearnProps) {
 
 interface ItemProps extends PlayToLearnItem {
   index: number;
+  activeIndex?: number;
+  isMobile: boolean;
 }
 const Item = (props: ItemProps) => {
-  const { index, img, bg, icon, title, description } = props;
+  const { index, activeIndex, isMobile, img, title, description } = props;
+  const isActive = index === activeIndex;
   return (
     <Flex
-      justifyContent="space-between"
-      direction={{
-        base: "column-reverse",
-        sm: index % 2 === 0 ? "row-reverse" : "row",
-      }}
       mt={10}
+      px={isActive ? 6 : 0}
+      align="center"
+      h={isMobile ? "fit-content" : 500}
     >
-      <Box w={{ base: "full", sm: "50%" }}>
-        <Image src={img} alt="Logo" width={800} height={800} />
-      </Box>
       <Flex
-        w={{ base: "full", sm: "50%" }}
-        direction="column"
-        justify="space-around"
-        pl={{ base: 0, sm: index % 2 === 0 ? 0 : 10 }}
+        justifyContent="space-between"
+        align="center"
+        direction={{
+          base: "column-reverse",
+          sm: index % 2 === 0 ? "row-reverse" : "row",
+        }}
+        h={isActive ? "full" : "fit-content"}
+        border="1px solid"
       >
+        <Box w={{ base: "full", sm: "50%" }}>
+          <Image src={img} alt="Logo" width={800} height={800} />
+        </Box>
         <Flex
+          w={{ base: "full", sm: "50%" }}
           direction="column"
-          justify="space-between"
-          w={{ base: "full", sm: "60%" }}
-          align={{ base: "center", sm: "flex-start" }}
-          textAlign={{ base: "center", sm: "left" }}
+          justify="space-around"
+          pl={{ base: 0, sm: index % 2 === 0 ? 0 : 10 }}
         >
-          <Image width={150} height={150} alt="Logo" src="/logo.svg" priority />
-          <Heading size="md" fontWeight="bold" my={2}>
-            {title}
-          </Heading>
-          <Text color="brandGray.500" mt={2}>
-            {description}
-          </Text>
+          <Flex
+            direction="column"
+            justify="space-between"
+            w={{ base: "full", sm: "60%" }}
+            align={{ base: "center", sm: "flex-start" }}
+            textAlign={{ base: "center", sm: "left" }}
+          >
+            <Image
+              width={150}
+              height={150}
+              alt="Logo"
+              src="/logo.svg"
+              priority
+            />
+            <Heading size="md" fontWeight="bold" my={2}>
+              {title}
+            </Heading>
+            <Text color="brandGray.500" mt={2}>
+              {description}
+            </Text>
+          </Flex>
         </Flex>
       </Flex>
     </Flex>
